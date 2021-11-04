a94d815f284fae8b4121191ab52a98240b0ac17e
wassim <wassimbouzakri@icloud.com>
2021-11-04 12:57:23 +0100

    :sparkles: add notification class

--- TD1/src/dispositif/Notification.java
+++ TD1/src/dispositif/Notification.java
@@ -1,4 +1,12 @@
 package dispositif;
 
 public class Notification {
+	private String	message;
+	private Integer id;
+	
+	public Notification(Integer id, String message) {
+		this.message=message;
+		this.id=id;
+		
+	}
 }
