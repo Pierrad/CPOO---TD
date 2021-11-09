71d4feaa56c18ad7879bd4237ff8ec03bc4962fd
florianbrrl <barrali.florian@gmail.com>
2021-11-09 11:12:49 +0100

    Update Alarme.java

--- TD1/src/dispositif/Alarme.java
+++ TD1/src/dispositif/Alarme.java
@@ -1,7 +1,16 @@
+package td2;
+
+
 package dispositif;
 
 public class Alarme extends Dispositif {
     public Alarme(String name, Integer id) {
         super(name, id);
     }
+
+    /* Méthode pour arreter l'alarme */ 
+    public void stop() {
+        //TODO
+    }
+    
 }
