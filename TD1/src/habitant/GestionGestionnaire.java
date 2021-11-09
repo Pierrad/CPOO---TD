0c9f146743de615ecbbd7dd9991058c7a9cc1d96
Joss COUPET <joss@macintosh.home>
2021-11-10 00:03:08 +0100

    :tada: try to do something for gestionHabitant

--- TD1/src/habitant/GestionGestionnaire.java
+++ TD1/src/habitant/GestionGestionnaire.java
@@ -1,4 +1,16 @@
 package habitant;
 
-public class GestionGestionnaire {
+public class GestionGestionnaire extends Habitant{
+
+    private boolean isGestionnaire = false;
+
+    public GestionGestionnaire(String name, String password) {
+        super(name, password);
+        this.isGestionnaire = true;
+    }
+    
+    public boolean getIsGestionnaire() {
+        return this.isGestionnaire;
+    }
+
 }
