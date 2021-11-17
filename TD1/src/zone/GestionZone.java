4073fd3924c9ebd4e83f375dd56115baa867aa79
Pierre-Adrien <pierreadrien.vasseur@gmail.com>
2021-11-17 17:40:02 +0100

    ✏️ Fix linting

--- TD1/src/zone/GestionZone.java
+++ TD1/src/zone/GestionZone.java
@@ -1,68 +1,64 @@
 package zone;
 
 import java.util.ArrayList;
-import zone.Zone;
 
 /**
  * Classe GestionZone
- * @author Marie ORFILA
  *
+ * @author Marie ORFILA
  */
 
-public class GestionZone
-{
+public class GestionZone {
     private ArrayList<Zone> listZones = new ArrayList<Zone>();
 
 
-    public GestionZone()
-    {
+    public GestionZone() {
         listZones = new ArrayList<Zone>();
     }
 
     /**
      * Renvoi la liste de toutes les zones
+     *
      * @return List<Zone>
      */
 
-    public ArrayList<Zone> getZones()
-    {
+    public ArrayList<Zone> getZones() {
         return listZones;
     }
 
     /**
      * Change la liste des zones
+     *
      * @param List<Zone>
      */
-    public void setZones(ArrayList<Zone> zone)
-    {
+    public void setZones(ArrayList<Zone> zone) {
         this.listZones = zone;
     }
 
     /**
      * Ajout une zone
+     *
      * @param nom
      * @param description
      */
-    public void addZone(String nom, String description)
-    {
+    public void addZone(String nom, String description) {
         ArrayList<Zone> listZones = this.getZones();
         listZones.add(new Zone(nom, description));
     }
 
     /**
      * Supprime une zone
+     *
      * @param zone
      */
-    public void removeZone(Zone zone)
-    {
+    public void removeZone(Zone zone) {
         ArrayList<Zone> listZones = this.getZones();
         listZones.remove(zone);
     }
 
     public ArrayList<Zone> list() {
-        for (Zone zone: this.getZones())
-        {
-        	System.out.println(zone.toString());
+        for (Zone zone : this.getZones()) {
+            System.out.println(zone.toString());
         }
         return this.getZones();
     }
