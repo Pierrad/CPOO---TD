a77bdce67ebf556b9e8df1cb90aba0c8ab718719
Pierre-Adrien <pierreadrien.vasseur@gmail.com>
2021-11-17 16:34:16 +0100

    :sparkles: Add Nina capteur

--- TD1/src/dispositif/Capteur.java
+++ TD1/src/dispositif/Capteur.java
@@ -1,7 +1,26 @@
 package dispositif;
 
 public class Capteur extends Dispositif {
-    public Capteur(String name, Integer id) {
+
+    private Integer battery;
+    private GestionAlerte gestionAlerte;
+
+    public Capteur(String name, Integer id, Integer battery, GestionAlerte gestionAlerte) {
         super(name, id);
+        this.battery = battery;
+        this.gestionAlerte = gestionAlerte;
     }
+
+    public Integer getBattery() {
+        return battery;
+    }
+
+    public void setBattery(Integer battery) {
+        this.battery = battery;
+    }
+
+    public GestionAlerte getGestionAlerte() { return gestionAlerte; }
+
+    public void setGestionAlerte(GestionAlerte gestionAlerte) { this.gestionAlerte = gestionAlerte; }
+
 }
